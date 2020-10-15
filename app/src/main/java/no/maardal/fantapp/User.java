a8cce692f3a8e4a1a112d32a7639fef3c96995b9
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-10-15 10:23:59 +0200

    Add User
    This commit will:
    Add user class to capture data from server, and to present to UI.

--- app/src/main/java/no/maardal/fantapp/User.java
+++ app/src/main/java/no/maardal/fantapp/User.java
@@ -1,4 +1,48 @@
 package no.maardal.fantapp;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class User {
+    String firstName;
+    String lastName;
+    String userid;
+
+    public User() {
+
+    }
+
+    public User(JSONObject jo) throws JSONException {
+        setUserid(jo.getString("userid"));
+        if (jo.has("firstName")) {
+            setFirstName(jo.getString("firstName"));
+        }
+        if (jo.has("lastName")) {
+            setLastName(jo.getString("lastName"));
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
 }
